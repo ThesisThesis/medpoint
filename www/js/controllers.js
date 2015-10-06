09816bbc1f853e5bbd156c01cf4baf9663cb531e
cheana <cheana123@gmail.com>
2015-10-06 12:42:12 +0800

    Map with Markers Commit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -97,35 +97,119 @@ angular.module('starter.controllers', [])
 })
 
  .controller('MapCtrl', function($scope, $ionicLoading, $compile) {
-      function initialize() {
-        var myLatlng = new google.maps.LatLng(8.485780, 124.656029);
+      //  function initialize() {
+
+      //   var locations = [
+      //       ['a', 8.484547, 124.650343],
+      //       ['b', 8.484292, 124.656223]
+
+      //   ];
+      //   var myLatlng = new google.maps.LatLng(43.07493,-89.381388);
         
-        var mapOptions = {
-          center: myLatlng,
-          zoom: 16,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("map"),
-            mapOptions);
+      //   var mapOptions = {
+      //     center: myLatlng,
+      //     zoom: 16,
+      //     mapTypeId: google.maps.MapTypeId.ROADMAP
+      //   };
+      //   var map = new google.maps.Map(document.getElementById("map"),
+      //       mapOptions);
+        
+      //   //Marker + infowindow + angularjs compiled ng-click
+      //   var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
+      //   var compiled = $compile(contentString)($scope);
+
+      //   var infowindow = new google.maps.InfoWindow({
+      //     content: compiled[0]
+      //   });
+
+      //   var marker, i; 
+      //   for (i = 0; i < locations.length; i++) { 
+      // marker = new google.maps.Marker({
+      //   position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+      //   map: map
+      //   });
+
+      //   google.maps.event.addListener(marker, 'click', (function(marker, i) {
+      //       return function() {  
+      //       infowindow.setContent(locations[i][0]);       
+      //     infowindow.open(map,marker);
+      //   }
+      //   })(marker, i));
+      // }
+
+      //   $scope.map = map;
+      // }
+      // google.maps.event.addDomListener(window, 'load', initialize);
+      
+      // $scope.centerOnMe = function() {
+      //   if(!$scope.map) {
+      //     return;
+      //   }
+
+      //   $scope.loading = $ionicLoading.show({
+      //     content: 'Getting current location...',
+      //     showBackdrop: false
+      //   });
+
+      //   navigator.geolocation.getCurrentPosition(function(pos) {
+      //     $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+      //     $scope.loading.hide();
+      //   }, function(error) {
+      //     alert('Unable to get location: ' + error.message);
+      //   });
+      // };
+      
+      // $scope.clickTest = function() {
+      //   alert('Example of infowindow with ng-click')
+      // };
+      
+function initialize() {
+
+        var locations = [
+      ['Rose Pharmacy', 8.486287, 124.655836, 4],
+      ['Rose Pharmacy 1', 8.484292, 124.656094, 5],
+      ['Rose Pharmacy 2', 8.484929, 124.650601, 3],
+      ['Rose Pharmacy 3', 8.486669, 124.650043, 2],
+      ['Rose Pharmacy 4', 8.477671, 124.649528, 1]
+    ];
+
+      //  var myLatlng = new google.maps.LatLng(43.07493,-89.381388);
+        
+        // var mapOptions = {
+        // //  center: myLatlng,
+        //   zoom: 16,
+        //   mapTypeId: google.maps.MapTypeId.ROADMAP
+        // };
+        var map = new google.maps.Map(document.getElementById("map"), {
+             zoom: 16,
+      center: new google.maps.LatLng(8.486839, 124.655879),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+
+        });
+          
         
         //Marker + infowindow + angularjs compiled ng-click
         var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
         var compiled = $compile(contentString)($scope);
 
-        var infowindow = new google.maps.InfoWindow({
-          content: compiled[0]
-        });
+        var infowindow = new google.maps.InfoWindow();
 
-        var marker = new google.maps.Marker({
-          position: myLatlng,
+        var marker, i;
+
+         for (i = 0; i < locations.length; i++) { 
+        marker = new google.maps.Marker({
+          position: new google.maps.LatLng(locations[i][1], locations[i][2]),
           map: map,
-        //  draggable: true;
-          title: 'Uluru (Ayers Rock)'
+         // title: 'Uluru (Ayers Rock)'
         });
 
-        google.maps.event.addListener(marker, 'click', function() {
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+          return function() {
+          infowindow.setContent(locations[i][0]);
           infowindow.open(map,marker);
-        });
+        }
+        })(marker, i));
+        }
 
         $scope.map = map;
       }
@@ -153,4 +237,5 @@ angular.module('starter.controllers', [])
         alert('Example of infowindow with ng-click')
       };
       
+
     });
---
+++ www/js/sampMap.js
@@ -0,0 +1,69 @@
+
+       .controller('MapCtrl', function($scope, $ionicLoading, $compile) {
+ function initialize() {
+
+        var locations = [
+            ['a', 8.484547, 124.650343],
+            ['b', 8.484292, 124.656223]
+
+        ];
+        var myLatlng = new google.maps.LatLng(43.07493,-89.381388);
+        
+        var mapOptions = {
+          center: myLatlng,
+          zoom: 16,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map"),
+            mapOptions);
+        
+        //Marker + infowindow + angularjs compiled ng-click
+        var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
+        var compiled = $compile(contentString)($scope);
+
+        var infowindow = new google.maps.InfoWindow({
+          content: compiled[0]
+        });
+
+        var marker, i; 
+        for (i = 0; i < locations.length; i++) { 
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+        });
+
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {  
+            infowindow.setContent(locations[i][0]);       
+          infowindow.open(map,marker);
+        }
+        })(marker, i));
+      }
+
+        $scope.map = map;
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+      
+      $scope.centerOnMe = function() {
+        if(!$scope.map) {
+          return;
+        }
+
+        $scope.loading = $ionicLoading.show({
+          content: 'Getting current location...',
+          showBackdrop: false
+        });
+
+        navigator.geolocation.getCurrentPosition(function(pos) {
+          $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+          $scope.loading.hide();
+        }, function(error) {
+          alert('Unable to get location: ' + error.message);
+        });
+      };
+      
+      $scope.clickTest = function() {
+        alert('Example of infowindow with ng-click')
+      };
+
+});
